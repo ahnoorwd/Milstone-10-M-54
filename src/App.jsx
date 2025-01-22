21f37bb93e1026628b161f4adbb0483e6966b2fc
ahmed nur <ahmednur103308@gmail.com>
2025-01-22 12:06:06 +0600

    Added thed second commit from the server site

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,43 @@ function App() {
     .then(res=>res.json())
     .then(data=>setusers(data))
    },[])
+  
+  const handleadduser =e=>{
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user ={name,email}
+    console.log(user);
+// there we from the data we send to the server site from the client site 
+    fetch('http://localhost:5000/users',{
+    method:'POST',
+    headers:{
+      'content-type': 'application/json'
+    },
+    body:JSON.stringify(user)
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      console.log('inside post response',data);
+    })
+  }
+
+
+
+
 
   return (
     <>
      
       <h1>Users menagment system </h1>
+      <form onSubmit={handleadduser}>
+
+      <input type="text"  name='name' placeholder='name' /><br /><p></p>
+      <input type="text"  name='email' placeholder='email' /><br /><p></p>
+      <input type="submit"  value="Addsubmit" />
+
+      </form>
       <h3>Total users are there : {users.length}</h3>
       
       <div>
